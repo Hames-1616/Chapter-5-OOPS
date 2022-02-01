09e6e4a936221fdfb938d6fedb8a02b886834ed9
Shah Haamid <80157961+Hames-1616@users.noreply.github.com>
2022-02-01 14:57:43 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+float circarea (float x, float y)
+{
+    float area = x * y * y;
+  cout << "Area is "<< area << endl;
+  return area;
+}
+
+int main()
+{
+    const float PI = 3.14159F;
+    float rad,arc;
+    cout<<"Enter the Radius"<<endl;
+    cin>>rad;
+    arc = circarea(PI,rad);
+    cout<<arc;
+
+
+}
